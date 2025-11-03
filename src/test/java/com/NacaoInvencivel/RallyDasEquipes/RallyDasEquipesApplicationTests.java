3e38d01a38c5eda041acb3c6df849a1eab716b0a
MisheruSama <michelgermano010@outlook.com>
2025-11-03 15:17:40 -0300

    commit inicial

---
+++ src/test/java/com/NacaoInvencivel/RallyDasEquipes/RallyDasEquipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NacaoInvencivel.RallyDasEquipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RallyDasEquipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
