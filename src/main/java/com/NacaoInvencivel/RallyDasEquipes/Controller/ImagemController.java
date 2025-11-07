3e1052a04c13123de87669a6de27d6c638e609c7
MisheruSama <michelgermano010@outlook.com>
2025-11-07 03:14:33 -0300

    teste5

--- src/main/java/com/NacaoInvencivel/RallyDasEquipes/Controller/ImagemController.java
+++ src/main/java/com/NacaoInvencivel/RallyDasEquipes/Controller/ImagemController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/imagem")
+@RequestMapping("/static/imagem")
 @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET})
 public class ImagemController {
 
