38174e00794e84ccf1045dff980114d4ee561ab9
MisheruSama <michelgermano010@outlook.com>
2026-04-14 20:11:25 -0300

    adição do Enum para que o hibernate leia o enum

--- src/main/java/com/NacaoInvencivel/RallyDasEquipes/domain/User/User.java
+++ src/main/java/com/NacaoInvencivel/RallyDasEquipes/domain/User/User.java
@@ -28,6 +28,7 @@ public class User implements UserDetails {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
     public User(String nome, String login, String password, UserRole role) {
